fbc387b9dd8af09a564f23ceae998710a45b375f
Leon Wong <leonwong0901@gmail.com>
2022-02-02 17:13:09 +0800

    fix warning

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
-import flv from 'flv.js';
 import StreamForm from './StreamForm';
 import _ from 'lodash';
 
@@ -15,7 +14,6 @@ class StreamShow extends React.Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
-    // this.buildPlayer(id);
   }
 
   onSubmit = (formValues) => {
