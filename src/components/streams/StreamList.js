58fabd07550c1d78b51e9c5b604ae6b4525eb0bf
Leon Wong <leonwong0901@gmail.com>
2022-02-02 23:17:32 +0800

    remove unused code

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 import Modal from '../Modal';
-import history from '../../history';
 
 class StreamList extends React.Component {
   constructor(props) {
